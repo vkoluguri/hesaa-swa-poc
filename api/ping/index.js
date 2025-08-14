bb5ff96208f4572f6e088086d83f7c4c7206b2a6
vkoluguri <vkoluguri@hesaa.org>
2025-08-14 10:18:45 -0400

    Update index.js

--- api/ping/index.js
+++ api/ping/index.js
@@ -1,3 +1,12 @@
 module.exports = async function (context, req) {
-  return { status: 200, body: JSON.stringify({ ok: true, message: "pong" }) };
+  context.log("Ping called");
+  context.res = {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: {
+      ok: true,
+      message: "pong",
+      time: new Date().toISOString()
+    }
+  };
 };
