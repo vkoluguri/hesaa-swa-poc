5e6e4e1e3d1fde8f53cd5289643a1c6f6d7e357b
vkoluguri <vkoluguri@hesaa.org>
2025-09-17 10:49:52 -0400

    Update Header.tsx

--- spa/src/components/Header.tsx
+++ spa/src/components/Header.tsx
@@ -602,7 +602,7 @@ function MobileItem({ item }: { item: NavNode }) {
         <a
           href={leaf.href}
           target={leaf.target}
-          className="block rounded-md px-3 py-2 text-[16px] text-slate-800 hover:bg-[#e3ecff] hover:text-blue-900 hover:border-l-2 hover:border-blue-600"
+          className="block rounded-md px-3 py-2 text-[16px] text-slate-900 hover:bg-[#e3ecff] hover:text-blue-900 hover:border-l-2 hover:border-blue-600"
         >
           {leaf.label}
         </a>
@@ -616,7 +616,7 @@ function MobileItem({ item }: { item: NavNode }) {
                 <a
                   href={child.href}
                   target={child.target}
-                  className="block rounded-md px-3 py-2 text-[16px] text-slate-800 hover:bg-[#dbe5f9] ont-normal focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-600"
+                  className="block rounded-md px-3 py-2 text-[16px] text-slate-900 hover:bg-[#dbe5f9] ont-normal focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-blue-600"
                 >
                   {child.label}
                 </a>
